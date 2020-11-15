58b28741d726378c58a91f5b9f74087ec977ad98
kulasekhar <thurlukulasekhar@gmail.com>
2020-11-15 20:03:23 +0530

    even number

---
+++ src/com/app/numbers/Even.java
@@ -0,0 +1,11 @@
+package com.app.numbers;
+
+public class Even {
+    public static void main(String[] args) {
+        for (int i = 1; i <=10 ; i++) {
+            if (i%2==0) {
+                System.out.print("  "+i);
+            }
+        }
+    }
+}
